8eec2059cb71b7c1074ab8260c84b48d25129460
puklipo <pcs.engineer.team@gmail.com>
2024-10-28 14:49:22 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
@@ -22,15 +23,29 @@ Route::get('callback', function (Request $request) {
         dd($request->all());
     }
 
-    /** @var \Laravel\Socialite\Two\User $user */
-    $user = Socialite::driver('bluesky')->user();
+    /** @var \Laravel\Socialite\Two\User $socialite_user */
+    $socialite_user = Socialite::driver('bluesky')->user();
 
     /** @var \Revolution\Bluesky\Session\OAuthSession $session */
-    $session = $user->session;
+    $session = $socialite_user->session;
 
     $request->session()->put('bluesky_session', $session);
 
     dump($session);
+
+    $user = User::updateOrCreate([
+        'did' => $socialite_user->id,
+    ], [
+        'name' => $session->displayName(),
+        'handle' => $session->handle(),
+        'issuer' => $session->issuer(),
+        'avatar' => $session->avatar(),
+        'refresh_token' => $session->refresh(),
+    ]);
+
+    auth()->login($user, remember: true);
+
+    return to_route('dashboard');
 })->name('bluesky.oauth.redirect');
 
 //Route::middleware('auth')->group(function () {
