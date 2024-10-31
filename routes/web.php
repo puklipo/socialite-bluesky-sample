2af9c3f0fbf63d54b6c753496cc75348c3e96d67
puklipo <pcs.engineer.team@gmail.com>
2024-10-31 09:39:08 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -69,8 +69,8 @@ Route::post('/first', function (Request $request) {
 })->middleware(['auth', 'verified'])->name('bsky.first');
 
 Route::post('/refresh', function (Request $request) {
-    $session = OAuthSession::create(session('bluesky_session'));
-    //$session = OAuthSession::create();
+    //$session = OAuthSession::create(session('bluesky_session'));
+    $session = OAuthSession::create();
 
     if ($session->tokenExpired()) {
         $session = OAuthSession::create([
